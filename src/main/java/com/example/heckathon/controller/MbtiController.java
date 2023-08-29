33e0289b75cb3763ec6066b6590fa6576664a261
Li5ht <bell1234795@gmail.com>
2023-08-30 02:07:14 +0900

    commit

--- src/main/java/com/example/heckathon/controller/MbtiController.java
+++ src/main/java/com/example/heckathon/controller/MbtiController.java
@@ -5,8 +5,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
+@WebServlet("/MBTIServlet")
 public class MbtiController {
 
     @GetMapping("/")
@@ -23,10 +30,53 @@ public class MbtiController {
     }
 
     private String calculateMbtiType(String[] answers) {
-        // Implement your MBTI type calculation logic here based on user's answers
-        // This is a simplified example; you can use a more sophisticated algorithm
-        // to map answers to MBTI types.
-        // E.g., Counting E's and I's, N's and S's, etc.
-        // Return the calculated MBTI type as a string.
+        int[] scores = new int[4];
+
+        // Loop through answers and calculate scores
+        for (String answer : answers) {
+            char[] answerChars = answer.toCharArray();
+            for (int i = 0; i < answerChars.length; i++) {
+                // For simplicity, assume 'a' and 'b' indicate opposing preferences
+                // and calculate scores for each preference dimension
+                if (answerChars[i] == 'a') {
+                    scores[i]++;
+                } else if (answerChars[i] == 'b') {
+                    scores[i]--;
+                }
+            }
+        }
+
+        // Determine the MBTI type based on scores
+        StringBuilder mbtiTypeBuilder = new StringBuilder();
+        for (int score : scores) {
+            if (score >= 2) {
+                mbtiTypeBuilder.append("I");
+            } else {
+                mbtiTypeBuilder.append("E");
+            }
+        }
+        for (int score : scores) {
+            if (score >= 2) {
+                mbtiTypeBuilder.append("N");
+            } else {
+                mbtiTypeBuilder.append("S");
+            }
+        }
+        for (int score : scores) {
+            if (score >= 2) {
+                mbtiTypeBuilder.append("F");
+            } else {
+                mbtiTypeBuilder.append("T");
+            }
+        }
+        for (int score : scores) {
+            if (score >= 2) {
+                mbtiTypeBuilder.append("P");
+            } else {
+                mbtiTypeBuilder.append("J");
+            }
+        }
+
+        return mbtiTypeBuilder.toString();
     }
 }

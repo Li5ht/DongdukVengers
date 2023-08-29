8befde5db16cddf6d40516143ee743a6cd2d15c3
Li5ht <bell1234795@gmail.com>
2023-08-29 23:00:15 +0900

    commit

--- src/main/java/com/example/heckathon/controller/MbtiController.java
+++ src/main/java/com/example/heckathon/controller/MbtiController.java
@@ -14,19 +14,20 @@ public class MbtiController {
         return "questions";
     }
 
-    @PostMapping("/result")
-    public String showResult(@RequestParam String[] answers, Model model) {
+    //@PostMapping("/result")
+   /* public String showResult(@RequestParam String[] answers, Model model) {
         // Calculate MBTI type based on answers
         String mbtiType = calculateMbtiType(answers);
         model.addAttribute("mbtiType", mbtiType);
         return "result";
-    }
+    }*/
 
-    private String calculateMbtiType(String[] answers) {
-        // Implement your MBTI type calculation logic here based on user's answers
-        // This is a simplified example; you can use a more sophisticated algorithm
-        // to map answers to MBTI types.
-        // E.g., Counting E's and I's, N's and S's, etc.
-        // Return the calculated MBTI type as a string.
-    }
+//    private String calculateMbtiType(String[] answers) {
+//        // Implement your MBTI type calculation logic here based on user's answers
+//        // This is a simplified example; you can use a more sophisticated algorithm
+//        // to map answers to MBTI types.
+//        // E.g., Counting E's and I's, N's and S's, etc.
+//        // Return the calculated MBTI type as a string.
+//        return;
+//    }
 }
